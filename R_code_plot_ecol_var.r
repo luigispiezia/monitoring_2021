f749873f16548d7706c935c2d6c418c234da1612
luigispiezia <77782913+luigispiezia@users.noreply.github.com>
2021-01-22 13:45:05 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -36,3 +36,59 @@ plot(cadmium, lead)
 pairs(meuse)
 
 #pairing only the elements art of the dataset:
+
+##################
+
+#lecture 2 on ecological variablese
+
+#exercise
+#recall the package sp, recall dataset meuse
+
+library(sp)
+data(meuse)
+
+#scatterplot matrics
+pairs(meuse)
+
+#view the very first part of the variables
+head(mouse)
+
+#cadmium copper lead zinc
+#pairs with soil variables --> seet what are the numbers of column in which this variables are present:
+#column 3,4,5,6
+
+pairs(meuse[,3:6])
+
+#let's use the name of the columns 
+#column cadmium+copper+lead+zinc alt+5= ~
+pairs(~cadmium + copper + lead + zinc, data= meuse)
+
+#let's prettify the graph
+
+#exercise: change the color.
+pairs(~cadmium + copper + lead + zinc, data= meuse, col=45)
+
+#for the future change colors of single panel by the par() function
+
+#change simbol to filled triangles
+pairs(~cadmium + copper + lead + zinc, data= meuse, col=45, pch=17)
+
+#increase the size of the triangles
+pairs(~cadmium + copper + lead + zinc, data= meuse, col=45, pch=17,cex=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
